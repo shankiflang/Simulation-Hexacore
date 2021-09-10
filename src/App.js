bbc2138a1dac3f27175dd9b6b6ae5b043e4c995c
Benjamin BENOIT <shankiflang@gmail.com>
2021-09-10 14:21:49 +0200

    next

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
 import { useEffect } from 'react';
+import leds from './plate.json';
 
 function App() {
     const pixelWeight = 30;
     const lineNmb = 22;
-    const lineMax = 8;
 
     const generate = () => {
         const canvas = document.getElementById('canvas');
         const ctx = canvas.getContext('2d');
 
-        let leds = Array(lineNmb).fill(Array(lineNmb).fill(0));
-
-        // Remove left-top corner
-        for (let a = 0; a < lineMax; a++) {
-            for (let b = lineMax; b >= 0; b--) {
-                console.log(`[${b}][${a}]`)
-                leds[a][b] = null;
-            }
-        }
-
         // All colors
         leds.forEach((x, xindex) => {
             x.forEach((y, yindex) => {
-                if (y != null) {
-                    let R = Math.floor(Math.random() * 255),
-                        G = Math.floor(Math.random() * 255),
-                        B = Math.floor(Math.random() * 255);
-
-                    ctx.fillStyle = `rgb(${R} , ${G}, ${B})`;
+                if (xindex === Math.floor(Math.random() * lineNmb) && yindex === lineNmb / Math.floor(Math.random() * lineNmb)) {
+                    console.log(x, y)
+                    ctx.fillStyle = `white`;
                     ctx.fillRect(
                         xindex * pixelWeight,
                         yindex * pixelWeight,
                         pixelWeight,
                         pixelWeight
                     );
+                } else {
+                    if (y != null) {
+                        let R = Math.floor(Math.random() * 255),
+                            G = Math.floor(Math.random() * 255),
+                            B = Math.floor(Math.random() * 255);
+
+                        ctx.fillStyle = `rgb(${R} , ${G}, ${B})`;
+                        ctx.fillRect(
+                            xindex * pixelWeight,
+                            yindex * pixelWeight,
+                            pixelWeight,
+                            pixelWeight
+                        );
+                    }
                 }
             });
         });
